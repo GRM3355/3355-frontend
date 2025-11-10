4f9dbfa8c4d03dbbe5edd5382dc5bad1c49ca192
6108 <6108@users.noreply.github.com>
2025-11-10 16:18:14 +0900

    fix: 로컬 api 경로 오류 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.PROD
     ? 'https://api.zony.kro.kr' // 배포용
-    : '/api', // 로컬 proxy용
+    : '/', // 로컬 proxy용
 });
 
 export default api;
