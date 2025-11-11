0a5f0dc6f9abe7307b27e34750b840a456314d82
6108 <6108@users.noreply.github.com>
2025-11-11 17:42:55 +0900

    style: Tab 가로 수정

--- src/components/common/Tab.tsx
+++ src/components/common/Tab.tsx
@@ -16,7 +16,7 @@ export default function Tab({ items, selected, onSelect }: TabProps) {
     <>
       <div className='flex h-max p-4 overflow-x-auto whitespace-nowrap scrollbar-hide'>
         {items.map((item: TabItem) => (
-          <div key={item.key} className="flex flex-col w-full h-max">
+          <div key={item.key} className="flex flex-col w-max h-max">
             <span
               className={`p-2 cursor-pointer
                       ${selected === item.key ? "text-text-brand h-9 label1-sb" : "text-text-primary label2-r"}`}
