f948ce8131f681b6aec380b19a896d4f7ad7587b
6108 <6108@users.noreply.github.com>
2025-11-18 12:37:49 +0900

    test: 로그인 리다이렉트 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -117,7 +117,7 @@ function App() {
             <Route path='echo' element={<EchoTest />} />
             <Route path='*' element={<NotFound />} />
             {/* <Route path='test' element={<ComponentTestPage />} /> //TODO 추후 삭제 */}
-            <Route path='/r ' element={<KakaoRedirectPage />} />
+            <Route path='/api/auth/kakao/callback ' element={<KakaoRedirectPage />} />
           </Routes>
         </div>
       </div>
