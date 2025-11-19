be0ac334a2b338ed7080274d6583b8dfcfe66eb8
6108 <6108@users.noreply.github.com>
2025-11-19 11:44:19 +0900

    feat: 지역 세로 스크롤, 클릭시 움직여지는 문제

--- src/components/common/Tab.tsx
+++ src/components/common/Tab.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export type TabItem = {
   key: string;
@@ -12,14 +12,33 @@ type TabProps = {
 }
 
 export default function Tab({ items, selected, onSelect }: TabProps) {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scroll = scrollRef.current;
+    if (!scroll) return;
+
+    const onWheel = (e: WheelEvent) => {
+      console.log("wheel fired", e.deltaY);
+      if (e.deltaY === 0) return; // 세로 스크롤 감지
+      e.preventDefault(); //패시브 모드에서는 금지
+      scroll.scrollLeft += e.deltaY; // 세로휠 가로로
+    };
+
+    scroll.addEventListener("wheel", onWheel, { passive: false });
+    return () => scroll.removeEventListener("wheel", onWheel);
+  }, []);
+
   return (
     <>
-      <div className='flex h-max p-4 overflow-x-auto whitespace-nowrap scrollbar-hide snap-x'>
+      <div
+        ref={scrollRef}
+        className='flex h-max m-4 overflow-x-auto whitespace-nowrap scrollbar-hide'>
         {items.map((item: TabItem) => (
           <div key={item.key} className="flex flex-col w-max h-max">
             <span
-              className={`p-2 cursor-pointer snap-start
-                      ${selected === item.key ? "text-text-brand h-9 label1-sb" : "text-text-primary label2-r"}`}
+              className={`p-2 cursor-pointer snap-start h-9 
+                      ${selected === item.key ? "text-text-brand label1-sb" : "text-text-primary label2-r"}`}
               onClick={() => onSelect(item.key)}>
               {item.label}</span>
             {selected === item.key && <span className="w-full h-1 bg-text-brand rounded-full" />}
