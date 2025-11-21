ca02c0a833b738c1f01f018361dad7be369ca065
6108 <6108@users.noreply.github.com>
2025-11-21 11:11:50 +0900

    fix: build

--- src/components/festival/RoomListSection.tsx
+++ src/components/festival/RoomListSection.tsx
@@ -1,12 +1,6 @@
-import { useGetFestivalByFestivalId, useGetRoomsByFestivalId } from "@/hooks/useFestival";
-import FestivalInfoModal from "@/components/main/FestivalInfoModal";
 import RoomItem from "@/components/room/RoomItem";
 import { useState, type MouseEvent } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import type { ChatRoomAPI, FestivalAPI, RoomAPI } from "@/types/api";
-import Header from "@/components/layout/Header";
-import AD from "@/components/common/AD";
-import { Info, Plus, X } from "@mynaui/icons-react";
 import Select from "../common/Select";
 import { useGetRoomsByToken } from "@/hooks/useRoom";
 import useAuthStore from "@/stores/useAuthStore";
@@ -21,8 +15,7 @@ const FILTER = [
   { key: "latest", label: "최신순" },
 ]
 
-export default function RoomListSection({ festivalData, roomDatas }: RoomListSectionProps) {
-  const [isShowFestivalModal, setShowFestivalModal] = useState<boolean>(false);
+export default function RoomListSection({ roomDatas }: RoomListSectionProps) {
   const [filter, setFilter] = useState<string>('participant');
 
   const { accessToken } = useAuthStore();
@@ -43,14 +36,6 @@ export default function RoomListSection({ festivalData, roomDatas }: RoomListSec
   return (
     <div className="flex flex-col h-full p-4 pt-0">
       <div className="pb-8">
-        {/* <p>{festivalId}</p> */}
-        {/* <p>{data}</p> */}
-        {/* <div className="flex gap-2">
-          <p className="title1-sb flex-1">{festivalData.title}</p>
-          <Info size={16} onClick={(e) => handleShowInfo(e)}
-            className="border rounded-full border-text-primary my-1.5" />
-        </div>
-        <p className="caption2-r text-text-tertiary">페스티벌 Zone 내에서 채팅이 가능합니다.</p> */}
       </div>
       <div className="flex gap-1 pb-3">
         <span className="title3-sb text-text-primary">단체 채팅방</span>
