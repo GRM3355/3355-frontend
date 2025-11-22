41e4bde83444dd04929c29bb0d94507e45754288
6108 <6108@users.noreply.github.com>
2025-11-22 18:37:42 +0900

    fix: 페스티벌 리스트에서 이전 페이지로 이동 시 지역 유지

--- src/pages/FestivalListPage.tsx
+++ src/pages/FestivalListPage.tsx
@@ -4,9 +4,20 @@ import Tab from '@/components/common/Tab';
 import FestivalListSection from '@/components/festival/FestivalListSection';
 import { regions } from '@/utils/map';
 import Nav from '@/components/layout/Nav';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function FestivalListPage() {
-  const [region, setRegion] = useState<string>('SEOUL');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+  const initialRegion = searchParams.get('region') || 'SEOUL';
+  const [region, setRegion] = useState<string>(initialRegion);
+
+  const handleSelectRegion = (newRegion: string) => {
+    setRegion(newRegion);
+    navigate(`?region=${newRegion}`, { replace: true });
+  };
 
   return (
     <>
@@ -14,12 +25,11 @@ export default function FestivalListPage() {
       <div className='flex-1 w-full h-full relative overflow-hidden'>
         <div className='flex flex-col h-full '>
           <div className='flex-1'>
-
             {/* 지역 필터링 */}
             <Tab
               items={regions}
               selected={region}
-              onSelect={setRegion}
+              onSelect={handleSelectRegion}
             />
           </div>
           <FestivalListSection region={region} />
